2cf2980add146fbe46a80aefedda5397cffdcbab
bilykb <brettbilyk@gmail.com>
2021-08-20 17:42:52 +0000

    if else statement added to handle errors and body of request

--- iss.js
+++ iss.js
@@ -2,7 +2,12 @@ const request = require('request');
 
 const fetchMyIP = function() {
   request('https://api.ipify.org/?format=json', function (error, response, body) {
-  
+    if (error) {
+      callback(error, null);
+    } else {
+      console.log(body);
+      callback(null, body);
+    }
   });
 }
 
