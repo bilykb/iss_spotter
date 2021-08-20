8a1ec51d3a3d3f8b8ceb1ce16b318db715935421
bilykb <brettbilyk@gmail.com>
2021-08-20 19:31:38 +0000

    added skeleton of fetchCoordsbyIP function

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const fetchMyIP = function(callback) {
-  request('https://api.ipify.org/?format=json', function (error, response, body) {
+  request('https://api.ipify.org/?format=json', function(error, response, body) {
     if (error) {
       callback(error, null);
       return;
@@ -10,10 +10,13 @@ const fetchMyIP = function(callback) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
-    }
+    } 
+    
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
-}
+};
 
 module.exports = {
   fetchMyIP
-}
+};
