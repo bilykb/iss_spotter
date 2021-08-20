dce6dd9a02facf070526bf801411f7756f081649
bilykb <brettbilyk@gmail.com>
2021-08-20 19:20:57 +0000

    adapted code to what LHL wanted

--- iss.js
+++ iss.js
@@ -1,12 +1,15 @@
 const request = require('request');
 
-const fetchMyIP = function() {
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json', function (error, response, body) {
     if (error) {
       callback(error, null);
-    } else {
-      console.log(body);
-      callback(null, body);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     }
   });
 }
