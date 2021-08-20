02a81ab6a03b001fbc2f513993494f74f0ec8e0e
bilykb <brettbilyk@gmail.com>
2021-08-20 21:42:45 +0000

    completed fetchCoordsByIP

--- iss.js
+++ iss.js
@@ -8,9 +8,9 @@ const fetchMyIP = function(callback) {
     }
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+      callback(msg, null);
       return;
-    } 
+    }
     
     const ip = JSON.parse(body).ip;
     callback(null, ip);
@@ -18,9 +18,22 @@ const fetchMyIP = function(callback) {
 };
 
 const fetchCoordsByIP = (ip, callback) => {
-  
-}
+  request(`https://freegeoip.app/json/${ip}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(msg, null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
 
 module.exports = {
+  fetchCoordsByIP,
   fetchMyIP
 };
