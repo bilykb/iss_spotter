1c82f77fe285de4bd2a8dd620c944666e9629c35
bilykb <brettbilyk@gmail.com>
2021-08-21 22:15:58 +0000

    functions added for error handling, as well as printing the fly over times to console

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-// const { fetchMyIP } = require('./iss');
-// const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work", error);
-//   }
-//   console.log(`It worked! Returned IP ${ip}.`);
-// })
+const { nextISSTimesForMyLocation } = require('./iss');
 
-
-// fetchCoordsByIP("209.89.80.154", (error, coordinates) => {
-//   if(error) {
-//     console.log(`It didn't work!, ${error}.`);
-//     return;
-//   }
-//   console.log("It worked! Returned coordinates: ", coordinates);
-// });
-
-fetchISSFlyOverTimes({ latitude: 50.9157, longitude: -113.892 }, (error, times) => {
-  if (error) {
-    console.log(`It didn't work!: Error ${error}.`);
-    return;
+const printPassTimes = passTimes => {
+  for (const times of Object.values(passTimes.response)) {
+    const dateConversation = new Date(0);
+    dateConversation.setUTCSeconds(times.risetime);
+    console.log(`Next pass at ${dateConversation.toUTCString()} for ${times.duration} seconds!`);
   }
-  console.log("It worked! Returned times: ", times);
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
 });
