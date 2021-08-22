30cfb1963552555d629e9debfed8eb8a45b59e00
bilykb <brettbilyk@gmail.com>
2021-08-22 19:54:13 +0000

    fetchCoordsIp function defined using promises

--- iss_promised.js
+++ iss_promised.js
@@ -1,9 +1,16 @@
 const request = require('request-promise-native');
 
 const fetchMyIp = function() {
+  return request('https://api.ipify.org/?format=json')
+}
+const fetchCoordsByIP = function(body) {
+  const ipAddress = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ipAddress}`)
 
 }
 
+
 module.exports = {
-  fetchMyIp
+  fetchMyIp,
+  fetchCoordsByIP
 }
