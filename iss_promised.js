464cd1ab78918c821a9cd48f8fa63ba7cc283fc8
bilykb <brettbilyk@gmail.com>
2021-08-22 20:12:04 +0000

    add function nextISSTimesForMyLocation, export only this function

--- iss_promised.js
+++ iss_promised.js
@@ -6,11 +6,23 @@ const fetchMyIp = function() {
 const fetchCoordsByIP = function(body) {
   const ipAddress = JSON.parse(body).ip;
   return request(`https://freegeoip.app/json/${ipAddress}`)
-
 }
 
+const fetchISSFlyOverTimes = function(body) {
+  const flyOverTimes = JSON.parse(body)
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${flyOverTimes.latitude}&lon=${flyOverTimes.longitude}`)
+}
+
+const nextISSTimesForMyLocation = function(body) {
+  return fetchmyIp()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => {
+    const { response } = JSON.parse(body);
+    return response;
+  });
+}
 
 module.exports = {
-  fetchMyIp,
-  fetchCoordsByIP
+  nextISSTimesForMyLocation
 }
